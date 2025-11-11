f8e61409231f6b33e205aa79d6ada119ec92ddb5
Vishnu4538 <edlavishnu@aol.com>
2025-11-11 06:33:33 +0000

    working fine of yaml

--- pythonscripts_for_common_task/demo.py
+++ pythonscripts_for_common_task/demo.py
@@ -1,3 +1,3 @@
 import devops_module as dev
 
-dev.update_image(100,"/workspaces/Vishnu-Projects/Projects-CICD/vishnu.yml")
+dev.update_image(101,"/workspaces/Vishnu-Projects/Projects-CICD/vishnu.yml")
